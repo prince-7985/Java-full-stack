9923735d2d41a520b8718a7c42d0a5b544e8d2dd
Prince Sharma <prince.sharma@qilegal.com>
2026-07-13 09:47:22 +0530

    day2

---
+++ ArrayTest.java
@@ -0,0 +1,10 @@
+public class ArrayTest {
+    public  static void main(String[] args){
+        String[] names = {"java", "python", "c++", "c#", "javascript"};
+        int index =0;
+        while(index<names.length){
+            System.out.println(names[index]);
+            index++;
+        }
+    }  
+}
