1c675e0edf6131d5c31de4548baed3e63c9127b9
Prince Sharma <prince.sharma@qilegal.com>
2026-07-15 10:13:05 +0530

    tranary operator

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class Ternary{
+    public static void main(String[] args){
+        int n = 7;
+        int result = 0;
+        result = (n%2==0)? 10: 15;
+        System.out.println(result);
+    }
+}
