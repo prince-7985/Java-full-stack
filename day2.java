98df7afeba31c035ccd2a282fc4b07ef3bdfefa3
Prince Sharma <prince.sharma@qilegal.com>
2026-07-15 10:04:47 +0530

    day3

---
+++ day2.java
@@ -0,0 +1,15 @@
+public class day2 {
+    public static void main(String[] args) {
+        int i=0;
+
+        while(true){    
+            System.out.println("Hello i am dev" + i);
+            if(i==5){
+                System.out.println("Stopping the loop");
+                break;
+            }
+            i++;
+        }
+        System.out.println("Ye loop ke bahar aa gaye");
+    }
+}
